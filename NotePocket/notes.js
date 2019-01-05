4af6af3e0d59d001ce190df31186550143bf49a5
Krupa Wojciech <wojciech.krupa@microsoft.wsei.edu.pl>
2019-01-05 17:01:34 +0100

    google maps i notatnik

---
+++ NotePocket/notes.js
@@ -0,0 +1,12 @@
+let Notatka = {
+    title: 'przykladowy tytul',
+    content: 'something to remember',
+    data: Date.now()
+}
+
+
+
+//JSON.parse
+//let jnote = JSON.stringify(Notatka)
+//localStorage.setItem("Notatki", jnote)
+//let a = JSON.parse(localStorage.getItem("notatki"))
